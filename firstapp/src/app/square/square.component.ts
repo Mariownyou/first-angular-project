a752b6061f47aad0a4ec0ac14bf7a01350950a6c
Leo Kondratev <39639000+Mariownyou@users.noreply.github.com>
2020-11-26 03:16:53 +0300

    есть стили

--- firstapp/src/app/square/square.component.ts
+++ firstapp/src/app/square/square.component.ts
@@ -3,9 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-square',
   template: `
-    <button>
+    <button *ngIf='value == "X"' mat-raised-button color="warn">
       {{ value }}
     </button>
+    <button *ngIf='value == "O"' mat-raised-button color="primary">
+      {{ value }}
+    </button>
+    <button *ngIf='!value' mat-raised-button></button>
   `,
   styles: ['button { width: 100%; height: 100%; font-size: 5em !important; }'
   ]
